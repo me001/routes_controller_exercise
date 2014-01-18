03ed8607ac4217a6649701a4a789de83e3594d85
me001 <mikeengel0425@gmail.com>
2014-01-18 09:31:46 +0800

    completed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,13 @@
 class ProductsController < ApplicationController
+  # GET /users/new
+  # GET /users/new.json
+  def new
+    @product = Product.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @product }
+    end
+  end
+
 end
